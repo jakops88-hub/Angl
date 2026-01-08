483b7055f06cce0a1e8ee01ad6f458d7ad7f7d2e
NorthernDev <jakops88@gmail.com>
2026-01-08 13:13:47 +0100

    Update target SDK to API 35 for Google Play Console compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.angl"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.angl"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
